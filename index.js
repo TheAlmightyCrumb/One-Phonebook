e0d64ca9adc40dad774c7ceed92858014acfa8e5
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-08-31 18:46:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,9 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
   
-  app.use(unknownEndpoint);
+app.use(unknownEndpoint);
 
-const PORT = 3001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+})
