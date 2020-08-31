af25e8a18aee4161797555f772ed1be39ec61d26
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-08-31 19:08:19 +0300

    basa

---
+++ build/precache-manifest.8b07f4e8b9bd440e9a6e5e2641d797c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2876b9e8c4e10e8d834ec00eea90cce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddced24e4677ff7bed19",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b760dc6156344fdb1767",
+    "url": "/static/js/2.7763b8f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7763b8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddced24e4677ff7bed19",
+    "url": "/static/js/main.d277f771.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 
 morgan.token('req-body', (req, res) => req.method === 'POST' ? JSON.stringify(req.body) : '');
 
-app.use(cors())
+app.use(express.static('./build'));
+app.use(cors());
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'));
 
